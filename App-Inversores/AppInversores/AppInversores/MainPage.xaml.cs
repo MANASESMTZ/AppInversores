2da1e46df63d89fe7b4dd57188d7761b71e7f59c
MANASESMTZ <121955806+MANASESMTZ@users.noreply.github.com>
2023-10-08 14:29:25 -0600

    Add files via upload

---
+++ App-Inversores/AppInversores/AppInversores/MainPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AppInversores
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            double inversor1 = 0;
+            double inversor2 = 0;
+            double inversor3 = 0;
+
+            double total = 0;
+
+            double resultado1 = 0;
+            double resultado2 = 0;
+            double resultado3 = 0;
+
+            inversor1 = Double.Parse(INVERSION1.Text);
+            inversor2 = Double.Parse(INVERSION2.Text);
+            inversor3 = Double.Parse(INVERSION3.Text);
+
+            total = inversor1 + inversor2 + inversor3;
+
+            resultado1 = (inversor1 / total) * 100;
+            resultado2 = (inversor2 / total) * 100;
+            resultado3 = (inversor3 / total) * 100;
+
+            await Navigation.PushAsync(new Page1(total.ToString("#.##"),resultado1.ToString("#.##"), resultado2.ToString("#.##"), resultado3.ToString("#.##")));
+
+        }
+    }
+}
